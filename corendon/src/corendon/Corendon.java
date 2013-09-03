279f647ae30ebc775e4547fc8809bfaa540fddc4
FunkeyFlo <florentijncornet@gmail.com>
2013-09-03 11:20:20 +0200

    Kippen

---
+++ corendon/src/corendon/Corendon.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corendon;
+
+/**
+ *
+ * @author Florentijn
+ */
+public class Corendon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Mark is kippen.");
+        // TODO code application logic here
+    }
+}
