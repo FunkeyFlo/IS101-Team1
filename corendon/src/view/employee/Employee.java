ab7bb31b9fe2aef9abdb528b1d52da173181bbea
FunkeyFlo <florentijncornet@gmail.com>
2013-10-29 11:00:01 +0100

    koekje onzin test

--- corendon/src/view/employee/Employee.java
+++ corendon/src/view/employee/Employee.java
@@ -1104,7 +1104,7 @@ public class Employee extends javax.swing.JFrame {
         for (int i = 0; i < correctInput.length; i++) {
             if (correctInput[i] == false) {
                 totalCorrectInput = false;
-//                System.out.println("false" + i); TESTBLABLA
+//                System.out.println("false" + i); koekje
                 break;
             }
             else {
